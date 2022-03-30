a28fa59abcc787e073d67946738e2e70d76a09fb
theoryNine <anselcolvin@gmail.com>
2022-03-30 00:41:38 -0700

    Add global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-
-import "./src/global-styles.css"
+import "./src/styles/global-styles.css"
+import "./src/styles/global-colors.css"
