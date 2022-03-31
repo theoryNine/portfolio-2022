7d750f4d72e5e7745f518a2ddfe6b91db3750158
theoryNine <anselcolvin@gmail.com>
2022-03-30 19:05:58 -0700

    Add font loaders

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Mulish\:200,200i,400,400i,700,700i`,
+          `Pacifico\:200,200i,400,400i,700,700i`
+        ],
+        display: 'swap'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,14 @@ import styled from "styled-components"
 
 const Headline = styled.h1`
   font-size: 35px;
+  font-family: 'Pacifico';
+  font-weight: 700;
 `
 
 const SubHeadline = styled.h2`
   font-size: 30px;
+  font-family: 'Mulish';
+  font-weight: 400;
 `
 
 const HeroContainer = styled.div`
@@ -14,6 +18,7 @@ const HeroContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  height: 600px;
 `
 
 const Hero = ({ headline, subhead }) => {
