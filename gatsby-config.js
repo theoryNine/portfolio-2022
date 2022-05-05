94249ffcc97ee2ad174c0fc74ccc113d75085df9
theoryNine <anselcolvin@gmail.com>
2022-05-05 13:18:04 -0700

    Update head

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
+        icon: 'src/images/logo.png',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
