85bc31ea1ccbdfb35a7c6c112f16dfc04eb96a6b
theoryNine <anselcolvin@gmail.com>
2022-05-09 10:49:18 -0700

    Updating analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Helmet } from "react-helmet"
 import Layout from "../components/layout";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
@@ -7,18 +6,11 @@ import Skills from "../components/Skills/Skills";
 import Work from "../components/Work/Work";
 import Contact from "../components/Contact/Contact";
 import Copyright from "../components/Copyright";
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-68163712-2');
-ReactGA.pageview(window.location.pathname + window.location.search);
+import Seo from "../components/seo";
 
 const IndexPage = () => (
   <>
-    <Helmet htmlAttributes={{lang : 'en'}}>
-      <meta charSet="utf-8" />
-      <title>Ansel Colvin | Front-End Developer</title>
-      <description>Front-End Developer, React Developer, and Certified Adobe Experience Manager developer. Making the web a better place.</description>
-    </Helmet>
+    <Seo />
     <Layout>
       <Hero />
       <About />
