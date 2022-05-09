29ae07a0f913e0a3112f24981c47ba8dba29119a
theoryNine <anselcolvin@gmail.com>
2022-05-09 11:09:59 -0700

    Update ana

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@ module.exports = {
     author: `Ansel Colvin`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-68163712-2", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
