7cbc84557d375e9f105395a7b9da3842382a9a7a
theoryNine <anselcolvin@gmail.com>
2022-05-09 08:44:21 -0700

    Remove analytics to re-do

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,26 +31,6 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "UA-68163712-1", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "YOUR_SELF_HOSTED_ORIGIN",
-        },
-      },
-    },
   ],
     {
       resolve: `gatsby-plugin-google-fonts`,
