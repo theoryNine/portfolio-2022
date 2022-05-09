c8e08327d560f1c415c6c2ce45ea4069b5bbeff5
theoryNine <anselcolvin@gmail.com>
2022-05-09 10:46:12 -0700

    Updating analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ import Skills from "../components/Skills/Skills";
 import Work from "../components/Work/Work";
 import Contact from "../components/Contact/Contact";
 import Copyright from "../components/Copyright";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-68163712-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const IndexPage = () => (
   <>
